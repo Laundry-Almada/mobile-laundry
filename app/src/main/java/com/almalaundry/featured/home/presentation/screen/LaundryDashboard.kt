1a503695e60d09ec83fe26104bc60ca73088b45a
AenGlinr <delladhela99@gmail.com>
2025-05-08 11:30:58 +0700

    Ubah ukuran grafik

--- app/src/main/java/com/almalaundry/featured/home/presentation/screen/LaundryDashboard.kt
+++ app/src/main/java/com/almalaundry/featured/home/presentation/screen/LaundryDashboard.kt
@@ -158,7 +158,7 @@ fun LaundryDashboard(
                             modifier = Modifier
                                 .padding(16.dp)
                                 .width(383.dp)
-                                .height(190.dp)
+                                .height(183.dp)
                                 .shadow(10.dp, RoundedCornerShape(20.dp))
                                 .background(
                                     MaterialTheme.colorScheme.surface,
@@ -191,7 +191,7 @@ fun LaundryDashboard(
                             modifier = Modifier
                                 .padding(16.dp)
                                 .width(383.dp)
-                                .height(190.dp)
+                                .height(183.dp)
                                 .shadow(10.dp, RoundedCornerShape(20.dp))
                                 .background(
                                     MaterialTheme.colorScheme.surface,
